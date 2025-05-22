1e98abcf7fb7b9609c7ed259191bdae2d23458be
Salih Emin <914656+cerebrux@users.noreply.github.com>
2025-05-22 15:47:45 +0300

    add world, update create

--- wallet_addr.sql
+++ wallet_addr.sql
@@ -17,6 +17,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `wallets`
 --
+CREATE DATABASE IF NOT EXISTS `wallets` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `wallets`;
 
 -- --------------------------------------------------------
 
